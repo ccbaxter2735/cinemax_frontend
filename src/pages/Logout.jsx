bbbe812f4d0e7aeaf57b4d48dcd88a4a57e766bc
terussar <erussard624@gmail.com>
2025-10-16 16:19:05 +0200

    ajout logout jsx

---
+++ src/pages/Logout.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+
+export default function Logout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    localStorage.removeItem(ACCESS_TOKEN);
+    localStorage.removeItem(REFRESH_TOKEN);
+    localStorage.removeItem("username");
+
+    navigate("/login");
+  }, [navigate]);
+
+  return null;
+}
